8a6fe5d004f9c6d9fd4dceaba7ff10a0c4414140
CsaMisi <misicsanko@gmail.com>
2026-04-26 14:17:24 +0200

    implement home to be the main point (router-outlet)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then(m => m.Home),
+    
+    children: [
+      {
+        path: '', 
+        loadComponent: () => import('./dashboard/dashboard').then(m => m.DashboardComponent),
+      },
+      {
+        path: 'character/:id',
+        loadComponent: () => import('./profile/profile').then(m => m.Profile),
+      }
+    ]
+  }
+];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DashboardComponent } from "../dashboard/dashboard";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [DashboardComponent],
+  imports: [RouterOutlet],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
