db9223ed1fea20fb51235acbf09877841d7969cb
CsaMisi <misicsanko@gmail.com>
2026-04-26 16:59:55 +0200

    implement sidebar service with mobile in mind ()

---
+++ src/app/core/services/sidebar.service.ts
@@ -0,0 +1,23 @@
+import { computed, Injectable, signal, Signal, WritableSignal } from "@angular/core";
+import { Sign } from "crypto";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class SidebarService{
+    readonly isOpen : WritableSignal<boolean> = signal<boolean>(true);
+    readonly mobileIsOpen : WritableSignal<boolean> = signal<boolean>(false); //Default to desktop
+
+    readonly state : Signal<"open" | "closed"> = computed(()=> this.isOpen() ? 'open' : 'closed')
+
+    ToggleSidebar(){
+        this.isOpen.update(open => !open)
+    }
+
+    ToggleMobileSidebar(){
+        this.mobileIsOpen.update(open => !open) //TODO: implement mobile sidebar if time allows
+    }
+
+}
